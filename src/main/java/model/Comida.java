ced02fd47d81ca46da119cf512b50f59873d0aa3
joaquinPega <joaquin_pega@hotmail.com>
2014-08-22 16:59:06 -0300

    hibernate

--- src/main/java/model/Comida.java
+++ src/main/java/model/Comida.java
@@ -3,11 +3,20 @@ package model;
 import java.util.List;
 
 public class Comida {
+	private int id;
 	List<Ingrediente> ingredientes;
 	List<Aderezo> aderezos;
 	
 	public Comida(){
 		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public List<Ingrediente> getIngredientes() {
